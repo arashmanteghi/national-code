59ea5f8e18eeac2ad2650b1db9690e67a42d5363
Arash Manteghi <arash.manteghi@gmail.com>
2018-02-11 17:56:36 +0330

    Complete Tests

--- src/NationalCode.test.js
+++ src/NationalCode.test.js
@@ -1,23 +1,47 @@
 import nationalCode from './NationalCode';
 
 describe('National code validation test', () => {
-  test('should return  true', () => {
+  test('should return true', () => {
+    expect(nationalCode(4160086731)).toBe(true);
+  });
+
+  test('should return false', () => {
+    expect(nationalCode(4160086732)).toBe(false);
+  });
+
+  test('should return true', () => {
     expect(nationalCode(4839941637)).toBe(true);
   });
 
-  test('should return  true', () => {
+  test('should return true', () => {
     expect(nationalCode('4839941637')).toBe(true);
   });
 
-  test('should return  false', () => {
+  test('should return false', () => {
     expect(nationalCode(8888888888)).toBe(false);
   });
 
-  test('should return  false', () => {
+  test('should return false', () => {
     expect(nationalCode(6838473293)).toBe(false);
   });
 
-  test('should return  false', () => {
+  test('should return false', () => {
     expect(nationalCode('6838473293')).toBe(false);
+  });
+
+  test('should return false', () => {
+    expect(nationalCode()).toBe(false);
+  });
+
+  test('should return false', () => {
+    expect(nationalCode(1234567542, '8364830361')).toBe(false);
+  });
+
+  test('should return false', () => {
+    expect(nationalCode(['1234873683'])).toBe(false);
+  });
+
+  test('should return false', () => {
+    expect(nationalCode(123456723542)).toBe(false);
   });
 });
